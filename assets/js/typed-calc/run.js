f6034e968702c6309c1ee5589aeb991b8e658972
mgechev <mgechev@gmail.com>
2017-08-07 11:10:05 -0700

    Fix text selection boundaries

--- assets/js/typed-calc/run.js
+++ assets/js/typed-calc/run.js
@@ -1727,45 +1727,17 @@ var Eval = function Eval(ast) {
 
 (function () {
 
-  function getCursorPos(input) {
-    if ("selectionStart" in input && document.activeElement == input) {
-      return {
-        start: input.selectionStart,
-        end: input.selectionEnd
-      };
-    }
-    else if (input.createTextRange) {
-      var sel = document.selection.createRange();
-      if (sel.parentElement() === input) {
-        var rng = input.createTextRange();
-        rng.moveToBookmark(sel.getBookmark());
-        for (var len = 0;
-          rng.compareEndPoints("EndToStart", rng) > 0;
-          rng.moveEnd("character", -1)) {
-          len++;
-        }
-        rng.setEndPoint("StartToStart", input.createTextRange());
-        for (var pos = { start: 0, end: len };
-          rng.compareEndPoints("EndToStart", rng) > 0;
-          rng.moveEnd("character", -1)) {
-          pos.start++;
-          pos.end++;
-        }
-        return pos;
-      }
-    }
-    return -1;
-  }
-
   document.getElementById('add-lambda-btn').onclick = function () {
-    var cp = getCursorPos(document.getElementById('code'));
+    var cp = document.getElementById('code').selectionStart;
     var val = document.getElementById('code').value;
     document.getElementById('code').value = val.substring(0, cp) + 'λ' + val.substring(cp, val.length);
+    document.getElementById('code').selectionStart = cp + 1;
   };
   document.getElementById('add-arrow-btn').onclick = function () {
-    var cp = getCursorPos(document.getElementById('code'));
+    var cp = document.getElementById('code').selectionStart;
     var val = document.getElementById('code').value;
     document.getElementById('code').value = val.substring(0, cp) + '→' + val.substring(cp, val.length);
+    document.getElementById('code').selectionStart = cp + 1;
   };
   document.getElementById('eval-btn').onclick = function () {
     var code = document.getElementById('code').value;
